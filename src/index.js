6bb7025759135083c83eb2b4270fa0238bbf8c62
majaliju <majalijunyc@gmail.com>
2022-02-28 15:48:00 -0500

    completed mostly but not stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', newTask)
+  });
+
+const newTask = function(event){
+  event.preventDefault()
+  let input = event.target.description.value
+  appendTask(input)
+  event.target.description.value = ' '
+}
+
+const appendTask = function(input){
+  // selectors, elements, variables
+  let ul = document.querySelector('#tasks')
+  let li = document.createElement('li')
+  let deleteBtn = document.createElement('button')
+  
+  // assign values
+  li.textContent = `${input} `
+  deleteBtn.innerText = 'X'
+
+  // appending to DOM 
+  li.append(deleteBtn)
+  ul.appendChild(li)
+}
